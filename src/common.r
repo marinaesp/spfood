c6548c771eba309703cc2212ba2f3405a4bfc734
Marina Espinasse <ecologM@hotmail.com>
2021-07-20 16:23:49 +0200

    added src folder and a template for html style

---
+++ src/common.r
@@ -0,0 +1,25 @@
+#common.R
+
+#round everything to 2 decimal places
+options(digits=3, scipen = 999)
+
+#libraries
+library(readxl)
+library(tidyverse)
+library(stringr)
+library(RColorBrewer)
+library(rgdal)
+library(raster)
+library(janitor)
+library(kableExtra)
+library(ggthemes)
+library(DT)
+library(tidyr)
+library(zoo)
+library(vroom)
+
+
+select <- dplyr::select
+
+
+
